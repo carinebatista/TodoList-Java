4eefcc4908eab7144482e7f0c4eff8d414f9bc91
Carine Batista <carinedcb@hotmail.com>
2023-10-16 22:51:42 -0300

    Adding integration to database

--- src/main/java/br/com/carinebatista/todolist/user/UserController.java
+++ src/main/java/br/com/carinebatista/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.carinebatista.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +24,17 @@ public class UserController {
 	/*
 	 * Body
 	 */
+	@Autowired
+	private IUserRepository userRepository;
+
 	@PostMapping("/")
-	public void create(@RequestBody UserModel userModel) {
-		System.out.println(userModel.name);
+	public ResponseEntity create(@RequestBody UserModel userModel) {
+		var user = this.userRepository.findByUsername(userModel.getUsername());
+
+		if (user != null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+		}
+		var userCreated = (this.userRepository.save(userModel));
+		return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 	}
 }
